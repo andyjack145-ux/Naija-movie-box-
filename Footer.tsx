e712000c0ddf7de1f2177c68839e693d31900240
andyjack145-ux <andyjack145@gmail.com>
2026-05-14 18:29:16 +0100

    Create Footer.tsx

---
+++ Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function Footer() {
+  return (
+    <footer className="p-6 text-center text-sm text-gray-500 border-t border-[#222]">
+      © 2026 9JA STREAM
+    </footer>
+  )
+}
