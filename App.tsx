590b4b5c9080bfc0afbdc9c53b0cf518e4749b28
andyjack145-ux <andyjack145@gmail.com>
2026-05-16 14:54:09 +0100

    Update and rename app.tsx to App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Routes,
   Route,
 } from 'react-router-dom'
@@ -13,6 +13,7 @@ import { Player } from './pages/Player'
 import { Downloads } from './pages/Downloads'
 import { Profile } from './pages/Profile'
 import { Upgrade } from './pages/Upgrade'
+import { Search } from './pages/Search'
 
 import { Navbar } from './components/layout/Navbar'
 import { Footer } from './components/layout/Footer'
@@ -31,13 +32,40 @@ export function App() {
             <main className="flex-1">
               <Routes>
                 <Route path="/" element={<Home />} />
+
                 <Route path="/login" element={<Login />} />
+
                 <Route path="/signup" element={<Signup />} />
-                <Route path="/movie/:id" element={<MovieDetail />} />
-                <Route path="/player/:id" element={<Player />} />
-                <Route path="/downloads" element={<Downloads />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/upgrade" element={<Upgrade />} />
+
+                <Route
+                  path="/movie/:id"
+                  element={<MovieDetail />}
+                />
+
+                <Route
+                  path="/player/:id"
+                  element={<Player />}
+                />
+
+                <Route
+                  path="/downloads"
+                  element={<Downloads />}
+                />
+
+                <Route
+                  path="/profile"
+                  element={<Profile />}
+                />
+
+                <Route
+                  path="/upgrade"
+                  element={<Upgrade />}
+                />
+
+                <Route
+                  path="/search"
+                  element={<Search />}
+                />
               </Routes>
             </main>
 
